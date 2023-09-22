fa2f88815c36d33960e4b0f6ec017e2af7886c51
TylerLo416 <lotyler416@gmail.com>
2023-09-21 18:54:12 -0700

    KokoEatingBananas

--- workspace/binarySearch/KokoEatingBananas.py
+++ workspace/binarySearch/KokoEatingBananas.py
@@ -1,9 +1,21 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         #Find max number in piles
-        
-        #create array k from 1 - max(piles)
-
+        #create array k from 1 to max(piles)
         #set up binary search through k
-
-        #for loop through piles[], check num hours required
+        max_piles = max(piles)
+        l, r = 1, max_piles
+        while l <= r:
+            m = l + (r - l) // 2
+            #for loop through piles[], check num hours required
+            numHours = 0
+            for i in piles:
+                numHours += (i+m-1)//m
+            #check until num hours is found
+            if numHours > h:#k is too small, iterate up
+                l = m+1
+            elif numHours <= h:#k is too big, iterate down
+                r = m-1
+            else:
+                return m
+        return l
