22dad96c8233f9a4269ac979d34b2ee15ec256bb
TylerLo416 <lotyler416@gmail.com>
2023-09-22 17:13:58 -0700

    Update MinimumRotatedSortedArray.py

--- workspace/binarySearch/MinimumRotatedSortedArray.py
+++ workspace/binarySearch/MinimumRotatedSortedArray.py
@@ -1,3 +1,14 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        
+        l, r = 0, len(nums)-1
+        minVal = float('inf')
+        while l <= r:
+            m = l + (r-l)//2
+            if m < minVal:
+                minVal = m
+            
+            if nums[m] < nums[l]:#check behind and ahead
+                r = m-1
+            elif nums[m] > nums[r]:
+                l = m+1
+        return minVal
